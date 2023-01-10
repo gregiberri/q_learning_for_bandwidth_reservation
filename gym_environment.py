85c0233672f587b868f9dcc783c0f833b219fbf0
gregibogyo <g.albert95@hotmail.com>
2023-01-10 23:00:31 +0100

    Tentative commit.

--- gym_environment.py
+++ gym_environment.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import os
-import tqdm
-import datetime as dt
 import gym
 from gym import spaces
 import numpy as np
@@ -9,17 +6,17 @@ import random
 
 from data_preprocess import load_and_preproces_datasets, train_val_split, PRICE_RESOLUTION
 
-MIN_BS_LENGTH = 600
-MAX_BS_LENGTH = 1200
+MIN_BS_LENGTH = 20
+MAX_BS_LENGTH = 40
 MIN_BS_NUMBER = 3
 MAX_BS_NUMBER = 10
 MIN_UNOV_BOOK = 0.5
 MAX_UNOV_BOOK = 5
 UNDER_EX_OVER_BOOK_RATES = [0, 1, 0]
 
-CANCELATION_FEE_PER_MINUTE = 0.00010  # per minutes
+CANCELATION_FEE_PER_MINUTE = 0  # per minutes
 CANCELATION_FEE = CANCELATION_FEE_PER_MINUTE / (60 / PRICE_RESOLUTION)
-CONSTRAINT_VIOLATION_COST = 1000
+CONSTRAINT_VIOLATION_COST = 1
 
 PRERESERVE_TIME = 1
 
@@ -36,8 +33,22 @@ class BREnv(gym.Env):
                  max_bs_number=MAX_BS_NUMBER,
                  min_unov_book=MIN_UNOV_BOOK,
                  max_unov_book=MAX_UNOV_BOOK,
-                 under_ex_over_book_rates=UNDER_EX_OVER_BOOK_RATES
+                 under_ex_over_book_rates=UNDER_EX_OVER_BOOK_RATES,
+                 random_seed: int = None
                  ):
+        """
+
+        Args:
+            no_df:
+            min_bs_length:
+            max_bs_length:
+            min_bs_number:
+            max_bs_number:
+            min_unov_book:
+            max_unov_book:
+            under_ex_over_book_rates:
+            random_seed:
+        """
         super(BREnv, self).__init__()
         # Define action and observation space
         # Discrete actions
@@ -46,7 +57,7 @@ class BREnv(gym.Env):
         self.observation_space = spaces.Box(low=0, high=1, shape=(1, 9), dtype=np.float16)
 
         # Reward range
-        # self.reward_range = (0, MAX_ACCOUNT_BALANCE)
+        self.reward_range = [-1, -0.3]
         self.no_df = no_df
 
         self.min_bs_length = min_bs_length
@@ -56,8 +67,19 @@ class BREnv(gym.Env):
         self.min_unov_book = min_unov_book
         self.max_unov_book = max_unov_book
         self.under_ex_over_book_rates = under_ex_over_book_rates
+        self.random_seed = random_seed
+
+    def set_random_seed(self):
+        """
+        Set the random seed: it should be None during training: hence all reset will yield different random numbers,
+        but should be a number for validation: hence the random numbers will be the same for all validation episode
+        """
+        np.random.seed(self.random_seed)
+        random.seed(self.random_seed)
 
     def reset(self):
+        self.set_random_seed()
+
         # Reset the state of the environment to an initial state
         self.current_bs = 0
         self.current_bs_step = 0
@@ -65,19 +87,23 @@ class BREnv(gym.Env):
         # Set the episode length
         self.bs_number = random.randint(self.min_bs_number, self.max_bs_number)
         self.bs_starts = np.random.randint(self.max_bs_number * self.max_bs_length + PRERESERVE_TIME,
-                                           len(self.no_df) - self.max_bs_number * self.max_bs_length + PRERESERVE_TIME, size=self.bs_number)
+                                           len(self.no_df) - self.max_bs_number * self.max_bs_length + PRERESERVE_TIME,
+                                           size=self.bs_number)
         self.bs_lengths = np.random.randint(self.min_bs_length, self.max_bs_length, size=self.bs_number)
 
         # Get for all the BS the current and the next prices
         self.bs_prices = []
         self.bs_future_prices = []
         for i in range(len(self.bs_lengths)):
-            self.bs_prices.append(self.no_df.loc[self.bs_starts[i]:self.bs_starts[i] + self.bs_lengths[i] - 1].reset_index())
+            self.bs_prices.append(
+                self.no_df.loc[self.bs_starts[i]:self.bs_starts[i] + self.bs_lengths[i] - 1].reset_index())
             if i == len(self.bs_lengths) - 1:  # last
                 random_start = random.randint(self.bs_lengths[i], len(self.no_df))
-                self.bs_future_prices.append(self.no_df.loc[random_start - self.bs_lengths[i]:random_start - 1].reset_index())
+                self.bs_future_prices.append(
+                    self.no_df.loc[random_start - self.bs_lengths[i]:random_start - 1].reset_index())
             else:
-                self.bs_future_prices.append(self.no_df.loc[self.bs_starts[i + 1] - self.bs_lengths[i]:self.bs_starts[i + 1] - 1].reset_index())
+                self.bs_future_prices.append(
+                    self.no_df.loc[self.bs_starts[i + 1] - self.bs_lengths[i]:self.bs_starts[i + 1] - 1].reset_index())
 
         # Get the index at the beginning at t0
         timesteps_from_start = np.cumsum(self.bs_lengths) - self.bs_lengths
@@ -102,19 +128,21 @@ class BREnv(gym.Env):
             else:
                 self.exunov_times.append(0)
 
-        return self._current_observation()
+        return self._get_obs()
 
-    def _current_observation(self):
+    def _get_obs(self):
         # get the current available prices
         self.current_prices = self.bs_prices[self.current_bs].loc[self.current_bs_step][['no1', 'no2', 'no3']].to_list()
-        self.future_prices = self.bs_future_prices[self.current_bs].loc[self.current_bs_step][['no1', 'no2', 'no3']].to_list()
+        self.future_prices = self.bs_future_prices[self.current_bs].loc[self.current_bs_step][
+            ['no1', 'no2', 'no3']].to_list()
         under_or_over = self.exunov_bookings[self.current_bs]
 
-        return [self.last_price] + self.current_prices + self.future_prices + [under_or_over, self.timestep_left_from_bs / MAX_BS_LENGTH]
+        return [self.last_price] + self.current_prices + self.future_prices + [under_or_over,
+                                                                               self.timestep_left_from_bs / MAX_BS_LENGTH]
 
     def step(self, action):
         cost = self._take_action(action)
-        reward = 1 - cost
+        reward = - cost
 
         # step to next
         self.current_bs_step += 1
@@ -125,18 +153,23 @@ class BREnv(gym.Env):
             # If it was the last bs`s last step we terminate the episode, only until the last-1 because we need one bs ahead for future bs price, maybe later new idea
             if self.current_bs == self.bs_number - 1:
                 self.current_bs_step -= 1
-                return self._current_observation(), reward, True, {}
+                # todo big negative reward if under or overbook, but it was not done
+                return self._get_obs(), reward, True, {}
 
             self.current_bs += 1
             self.current_bs_step = 0
             self.last_price = self.prereserved_prices[self.current_bs][0]
             self.timestep_left_from_bs = self.bs_lengths[self.current_bs]
 
-        obs = self._current_observation()
+        obs = self._get_obs()
 
         return obs, reward, False, {}
 
     def _take_action(self, action):
+        if self.random_seed is not None:
+            if self.last_price != np.min(self.current_prices):
+                asd = 1  # todo delete
+
         cost = self.last_price
 
         if action == 0:  # do nothing
@@ -153,7 +186,8 @@ class BREnv(gym.Env):
             cost += CANCELATION_FEE
 
         elif action in [4, 5, 6]:  # update reservation to first no
-            if self.exunov_bookings[self.current_bs] == 0:  # exact booking, but we would like to solve the underbooking scenario
+            if self.exunov_bookings[
+                self.current_bs] == 0:  # exact booking, but we would like to solve the underbooking scenario
                 cost += CONSTRAINT_VIOLATION_COST
             else:
                 cost += CANCELATION_FEE
@@ -171,17 +205,29 @@ class BREnv(gym.Env):
 
                     # set the prices to the underbooking
                     self.bs_prices[self.current_bs] = self.no_df.loc[self.bs_prices[self.current_bs]['index'].iloc[0]:
-                                                                     self.bs_prices[self.current_bs]['index'].iloc[-1] + self.exunov_times[self.current_bs]].reset_index()
-                    self.bs_prices[self.current_bs + 1] = self.no_df.loc[self.bs_prices[self.current_bs + 1]['index'].iloc[0] + self.exunov_times[self.current_bs]:
-                                                                         self.bs_prices[self.current_bs + 1]['index'].iloc[-1]].reset_index()
+                                                                     self.bs_prices[self.current_bs]['index'].iloc[-1] +
+                                                                     self.exunov_times[self.current_bs]].reset_index()
+                    self.bs_prices[self.current_bs + 1] = self.no_df.loc[
+                                                          self.bs_prices[self.current_bs + 1]['index'].iloc[0] +
+                                                          self.exunov_times[self.current_bs]:
+                                                          self.bs_prices[self.current_bs + 1]['index'].iloc[
+                                                              -1]].reset_index()
 
-                    self.bs_future_prices[self.current_bs] = self.no_df.loc[self.bs_future_prices[self.current_bs]['index'].iloc[0]:
-                                                                            self.bs_future_prices[self.current_bs]['index'].iloc[-1] + self.exunov_times[self.current_bs]].reset_index()
-                    self.bs_future_prices[self.current_bs + 1] = self.no_df.loc[self.bs_future_prices[self.current_bs + 1]['index'].iloc[0] + self.exunov_times[self.current_bs]:
-                                                                                self.bs_future_prices[self.current_bs + 1]['index'].iloc[-1]].reset_index()
+                    self.bs_future_prices[self.current_bs] = self.no_df.loc[
+                                                             self.bs_future_prices[self.current_bs]['index'].iloc[0]:
+                                                             self.bs_future_prices[self.current_bs]['index'].iloc[-1] +
+                                                             self.exunov_times[self.current_bs]].reset_index()
+                    self.bs_future_prices[self.current_bs + 1] = self.no_df.loc[
+                                                                 self.bs_future_prices[self.current_bs + 1][
+                                                                     'index'].iloc[0] + self.exunov_times[
+                                                                     self.current_bs]:
+                                                                 self.bs_future_prices[self.current_bs + 1][
+                                                                     'index'].iloc[-1]].reset_index()
 
-                    self.prereserved_prices[self.current_bs] = self.prereserved_prices[self.current_bs] + [new_prereserve_price] * self.exunov_times[self.current_bs]
-                    self.prereserved_prices[self.current_bs + 1] = self.prereserved_prices[self.current_bs + 1][self.exunov_times[self.current_bs]:]
+                    self.prereserved_prices[self.current_bs] = self.prereserved_prices[self.current_bs] + [
+                        new_prereserve_price] * self.exunov_times[self.current_bs]
+                    self.prereserved_prices[self.current_bs + 1] = self.prereserved_prices[self.current_bs + 1][
+                                                                   self.exunov_times[self.current_bs]:]
 
                 elif self.exunov_bookings[self.current_bs] == 1:  # overbooking
                     if action == 4:
@@ -197,20 +243,33 @@ class BREnv(gym.Env):
 
                     # set the prices to the over
                     self.bs_prices[self.current_bs] = self.no_df.loc[self.bs_prices[self.current_bs]['index'].iloc[0]:
-                                                                     self.bs_prices[self.current_bs]['index'].iloc[-1] - self.exunov_times[self.current_bs]].reset_index()
-                    self.bs_prices[self.current_bs + 1] = self.no_df.loc[self.bs_prices[self.current_bs + 1]['index'].iloc[0] - self.exunov_times[self.current_bs]:
-                                                                         self.bs_prices[self.current_bs + 1]['index'].iloc[-1]].reset_index()
+                                                                     self.bs_prices[self.current_bs]['index'].iloc[-1] -
+                                                                     self.exunov_times[self.current_bs]].reset_index()
+                    self.bs_prices[self.current_bs + 1] = self.no_df.loc[
+                                                          self.bs_prices[self.current_bs + 1]['index'].iloc[0] -
+                                                          self.exunov_times[self.current_bs]:
+                                                          self.bs_prices[self.current_bs + 1]['index'].iloc[
+                                                              -1]].reset_index()
 
-                    self.bs_future_prices[self.current_bs] = self.no_df.loc[self.bs_future_prices[self.current_bs]['index'].iloc[0]:
-                                                                            self.bs_future_prices[self.current_bs]['index'].iloc[-1] - self.exunov_times[self.current_bs]].reset_index()
-                    self.bs_future_prices[self.current_bs + 1] = self.no_df.loc[self.bs_future_prices[self.current_bs + 1]['index'].iloc[0] - self.exunov_times[self.current_bs]:
-                                                                                self.bs_future_prices[self.current_bs + 1]['index'].iloc[-1]].reset_index()
+                    self.bs_future_prices[self.current_bs] = self.no_df.loc[
+                                                             self.bs_future_prices[self.current_bs]['index'].iloc[0]:
+                                                             self.bs_future_prices[self.current_bs]['index'].iloc[-1] -
+                                                             self.exunov_times[self.current_bs]].reset_index()
+                    self.bs_future_prices[self.current_bs + 1] = self.no_df.loc[
+                                                                 self.bs_future_prices[self.current_bs + 1][
+                                                                     'index'].iloc[0] - self.exunov_times[
+                                                                     self.current_bs]:
+                                                                 self.bs_future_prices[self.current_bs + 1][
+                                                                     'index'].iloc[-1]].reset_index()
 
-                    self.prereserved_prices[self.current_bs] = self.prereserved_prices[self.current_bs][:-self.exunov_times[self.current_bs]]
-                    self.prereserved_prices[self.current_bs + 1] = [new_prereserve_price] * self.exunov_times[self.current_bs] + self.prereserved_prices[self.current_bs + 1]
+                    self.prereserved_prices[self.current_bs] = self.prereserved_prices[self.current_bs][
+                                                               :-self.exunov_times[self.current_bs]]
+                    self.prereserved_prices[self.current_bs + 1] = [new_prereserve_price] * self.exunov_times[
+                        self.current_bs] + self.prereserved_prices[self.current_bs + 1]
 
                     # todo at the last BS no cancelation fee
-                self.exunov_bookings[self.current_bs] = 0  # the under or overbooking is solved, so there is not under or overbooking
+                self.exunov_bookings[
+                    self.current_bs] = 0  # the under or overbooking is solved, so there is not under or overbooking
 
         return cost
 
--- q-learning.py
+++ q-learning.py
@@ -1,52 +1,45 @@
-import gym
-import torch, numpy as np
-from torch import nn
+import torch
+from gym.wrappers import TimeLimit
+from tianshou.env import DummyVectorEnv
 import tianshou as ts
 
 from data_preprocess import load_and_preproces_datasets, train_val_split
-from gym_environment import BREnv
-
-
-class Net(nn.Module):
-    def __init__(self, state_shape, action_shape):
-        super().__init__()
-        self.model = nn.Sequential(
-            nn.Linear(np.prod(state_shape), 128), nn.ReLU(inplace=True),
-            nn.Linear(128, 128), nn.ReLU(inplace=True),
-            nn.Linear(128, 128), nn.ReLU(inplace=True),
-            nn.Linear(128, np.prod(action_shape)),
-        )
-
-    def forward(self, obs, state=None, info={}):
-        if not isinstance(obs, torch.Tensor):
-            obs = torch.tensor(obs, dtype=torch.float)
-        batch = obs.shape[0]
-        logits = self.model(obs.view(batch, -1))
-        return logits, state
-
+from gym_environment import BREnv, MAX_BS_LENGTH
+from tianshou.utils.net.common import Net
 
 if __name__ == '__main__':
     no_df = load_and_preproces_datasets()
     no_df_train, no_df_val = train_val_split(no_df)
     no_df_train, no_df_val = no_df_train.reset_index(drop=True), no_df_val.reset_index(drop=True)
-    env = BREnv(no_df_train)
-    test_env = BREnv(no_df_val)
+    env = TimeLimit(BREnv(no_df_train))
+    train_env = DummyVectorEnv([lambda: TimeLimit(BREnv(no_df_train), 15000) for _ in range(10)])
+    test_env = DummyVectorEnv([lambda: TimeLimit(BREnv(no_df_val, random_seed=i), 15000) for i in range(1)])
 
     state_shape = env.observation_space.shape or env.observation_space.n
     action_shape = env.action_space.shape or env.action_space.n
-    net = Net(state_shape, action_shape)
-    optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+    # model
+    net = Net(state_shape,
+              action_shape,
+              hidden_sizes=[128, 128, 128, 128],
+              device='cuda',
+              # dueling=(Q_param, V_param),
+              ).cuda()
+    optim = torch.optim.Adam(net.parameters(), lr=1e-4, weight_decay=1e-5)
 
     policy = ts.policy.DQNPolicy(net, optim, discount_factor=0.9, estimation_step=3, target_update_freq=300)
 
-    train_collector = ts.data.Collector(policy, env, ts.data.VectorReplayBuffer(20000, 10), exploration_noise=True)
-    test_collector = ts.data.Collector(policy, test_env, ts.data.VectorReplayBuffer(20000, 10), exploration_noise=True)
+    train_collector = ts.data.Collector(policy, train_env, ts.data.VectorReplayBuffer(20000, len(train_env)),
+                                        exploration_noise=True)
+    test_collector = ts.data.Collector(policy, test_env, ts.data.VectorReplayBuffer(20000, len(test_env)),
+                                       exploration_noise=True)
 
+    print('Start training.')
     result = ts.trainer.offpolicy_trainer(
         policy, train_collector, test_collector,
-        max_epoch=10, step_per_epoch=10000, step_per_collect=10,
-        update_per_step=0.1, episode_per_test=100, batch_size=2,
+        max_epoch=30, step_per_epoch=1000 * MAX_BS_LENGTH, step_per_collect=100,
+        update_per_step=0.1, episode_per_test=1, batch_size=128,
         train_fn=lambda epoch, env_step: policy.set_eps(0.5),
-        test_fn=lambda epoch, env_step: policy.set_eps(0.05),
-        verbose=True, show_progress=True, test_in_train=False)
+        test_fn=lambda epoch, env_step: policy.set_eps(0.0),
+        verbose=True, show_progress=True, test_in_train=True
+    )
     print(f'Finished training! Use {result["duration"]}')
