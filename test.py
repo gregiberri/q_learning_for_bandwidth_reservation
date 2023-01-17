e9acd89bd4a8aef2950044932f15799ffd17c5d2
albert <albert@hyperplane.hu>
2023-01-18 00:02:33 +0100

    Update train and val scripts.

--- test.py
+++ test.py
@@ -2,6 +2,39 @@ from gym_environment import BREnv
 from q_learning import test_baselines, test_network, load_dfs
 
 _, no_df_val = load_dfs()
-test_env = BREnv(no_df_val, random_seed=1)
-test_baselines(test_env)
-test_network(test_env, 'fc', network_path='results/double_dqn.pth', result_path='results/double_dqn_policy.csv')
+
+# exact booking
+print('\nExact Booking')
+booking = 'exactbooking'
+test_env = BREnv(no_df_val, random_seed=1, under_ex_over_book_rates=[0, 1, 0], min_bs_number=10, max_bs_number=10)
+test_baselines(test_env, result_folder=f'results/{booking}')
+test_network(test_env, 'fc', network_path=f'results/{booking}/double_dqn.pth', result_path=f'results/{booking}/double_dqn_policy.csv,old.old')
+test_network(test_env, 'fc', network_path=f'results/{booking}/dqn.pth', result_path=f'results/{booking}/dqn_policy.csv')
+test_network(test_env, 'dueling_fc', network_path=f'results/{booking}/dueling_dqn.pth', result_path=f'results/{booking}/dueling_dqn_policy.csv')
+#
+# # underbooking
+print('\nUnder Booking')
+booking = 'underbooking'
+test_env = BREnv(no_df_val, random_seed=1, under_ex_over_book_rates=[1, 0, 0], min_bs_number=10, max_bs_number=10)
+test_baselines(test_env, result_folder=f'results/{booking}')
+test_network(test_env, 'fc', network_path=f'results/{booking}/double_dqn.pth', result_path=f'results/{booking}/double_dqn_policy.csv,old.old')
+test_network(test_env, 'fc', network_path=f'results/{booking}/dqn.pth', result_path=f'results/{booking}/dqn_policy.csv')
+test_network(test_env, 'dueling_fc', network_path=f'results/{booking}/dueling_dqn.pth', result_path=f'results/{booking}/dueling_dqn_policy.csv')
+
+# overbooking
+print('\nOver Booking')
+booking = 'overbooking'
+test_env = BREnv(no_df_val, random_seed=1, under_ex_over_book_rates=[0, 0, 1], min_bs_number=10, max_bs_number=10)
+test_baselines(test_env, result_folder=f'results/{booking}')
+test_network(test_env, 'fc', network_path=f'results/{booking}/double_dqn.pth', result_path=f'results/{booking}/double_dqn_policy.csv,old.old')
+test_network(test_env, 'fc', network_path=f'results/{booking}/dqn.pth', result_path=f'results/{booking}/dqn_policy.csv')
+test_network(test_env, 'dueling_fc', network_path=f'results/{booking}/dueling_dqn.pth', result_path=f'results/{booking}/dueling_dqn_policy.csv')
+
+# mixed booking
+print('\nMixed Booking')
+booking = 'mixedbooking'
+test_env = BREnv(no_df_val, random_seed=1, under_ex_over_book_rates=[1 / 3, 1 / 3, 1 / 3], min_bs_number=10, max_bs_number=10)
+test_baselines(test_env, result_folder=f'results/{booking}')
+test_network(test_env, 'fc', network_path=f'results/{booking}/double_dqn.pth', result_path=f'results/{booking}/double_dqn_policy.csv,old.old')
+test_network(test_env, 'fc', network_path=f'results/{booking}/dqn.pth', result_path=f'results/{booking}/dqn_policy.csv')
+test_network(test_env, 'dueling_fc', network_path=f'results/{booking}/dueling_dqn.pth', result_path=f'results/{booking}/dueling_dqn_policy.csv')
--- train.py
+++ train.py
@@ -1,4 +1,17 @@
 from q_learning import train
 
 train(network='fc',
-      save_path='results/double_dqn_new.pth')
+      save_path='results/mixedbooking/double_dqn.pth',
+      under_ex_over_book_rates=[1 / 3, 1 / 3, 1 / 3])
+
+train(network='fc',
+      save_path='results/exactbooking/dqn.pth',
+      under_ex_over_book_rates=[0, 1, 0])
+
+train(network='fc',
+      save_path='results/underbooking/double_dqn.pth',
+      under_ex_over_book_rates=[1, 0, 0])
+
+train(network='fc',
+      save_path='results/overbooking/dqn.pth',
+      under_ex_over_book_rates=[0, 0, 1])
